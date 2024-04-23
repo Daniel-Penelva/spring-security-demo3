374e0139bab48eea62fc65c67d133ec2da904c08
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-23 02:12:29 -0300

    criando projeto spring boot e spring security

---
+++ src/test/java/com/apirest/springsecuritydemo3/SpringSecurityDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.springsecuritydemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
